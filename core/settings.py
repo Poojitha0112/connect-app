96883d9e366b25a0189ead4bea78560d15d19dce
Poojitha Gaddamadugu <poojitha0112@gmail.com>
2023-03-12 22:33:37 +0530

    changes made for deploy

--- core/settings.py
+++ core/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   # 'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+     #   'NAME': BASE_DIR / 'db.sqlite3',
+   # }
 }
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
